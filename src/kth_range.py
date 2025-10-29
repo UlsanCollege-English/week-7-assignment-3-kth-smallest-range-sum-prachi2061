4365494dba46df3ca39b46817dc7cfc62f7aec2e
prachi2061 <prachistha1@gmail.com>
2025-10-29 14:38:07 +0900

    Add kth_range.py with insert, kth_smallest, and range_sum_bst functions

--- src/kth_range.py
+++ src/kth_range.py
@@ -5,13 +5,35 @@ class Node:
         self.right = None
 
 def insert(root, key):
-    # TODO: return new root after insert (reject duplicates)
-    pass
+    if root is None:
+        return Node(key)
+    if key == root.key:
+        return root  # Reject duplicates
+    elif key < root.key:
+        root.left = insert(root.left, key)
+    else:
+        root.right = insert(root.right, key)
+    return root
 
 def kth_smallest(root, k):
-    # TODO
-    pass
+    def inorder(node):
+        if node is None:
+            return []
+        return inorder(node.left) + [node.key] + inorder(node.right)
+
+    sorted_keys = inorder(root)
+    if k <= 0 or k > len(sorted_keys):
+        raise IndexError("k is out of bounds")
+    return sorted_keys[k - 1]
 
 def range_sum_bst(root, low, high):
-    # TODO
-    pass
+    if root is None:
+        return 0
+    if root.key < low:
+        return range_sum_bst(root.right, low, high)
+    elif root.key > high:
+        return range_sum_bst(root.left, low, high)
+    else:
+        return (root.key +
+                range_sum_bst(root.left, low, high) +
+                range_sum_bst(root.right, low, high))
